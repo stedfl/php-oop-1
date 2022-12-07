835276d2a44efff0059d3d12029ceba7cab92041
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-07 17:59:42 +0100

    add some style minor changes

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ include __DIR__  . '/db.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="img/favicon.ico" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
   <link rel="stylesheet" href="css/style.css">
   <title>Movies</title>
 </head>
@@ -33,12 +35,25 @@ include __DIR__  . '/db.php';
               <h3><?php echo $movie->original_title ?></h3>
               <p>"<?php echo $movie->description ?>"</p>
             </div>
-            <div class="info px-4">
+            <div class="info px-3">
               <ul>
-                <li>Cast: <?php echo $movie->crew->get_cast_list() ?>  </li>
-                <li>Regista: <?php echo $movie->crew->director ?> </li>
-                <li>Produttore: <?php echo $movie->crew->production ?? 'Non disponibile' ?></li>
-                <li>Casa di Distribuzione: <?php echo $movie->crew->distribution ?? 'Non disponibile' ?></li>
+                <li class="d-flex">
+                  <span class="me-2"><i class="fa-solid fa-people-group icon"></i></span> 
+                  <span>Cast: <?php echo $movie->crew->get_cast_list() ?></span>
+                   
+                </li>
+                <li class="d-flex">
+                  <span class="me-2"><i class="fa-solid fa-video icon"></i></i></span>
+                  <span>Regista: <?php echo $movie->crew->director ?></span>
+                </li>
+                <li class="d-flex">
+                  <span class="me-2"><i class="fa-solid fa-money-bill-wave icon"></i></span>
+                  <span>Produttore: <?php echo $movie->crew->production ?? 'Non disponibile' ?></span>
+                </li>
+                <li class="d-flex">
+                  <span class="me-2"><i class="fa-solid fa-building icon"></i></span>
+                  <span>Casa di Distribuzione: <?php echo $movie->crew->distribution ?? 'Non disponibile' ?></span>
+                </li>
               </ul>
             </div>
           </div>
