24bf747d83509672b91222d1412cf5713b238296
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-07 15:08:33 +0100

    create a class and objects of this class. Print them with var_dump

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+class Movie {
+  public $title;
+  public $original_title;
+  public $genre;
+  public $year;
+  public $poster = "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg";
+  public $description;
+
+  /**
+   * @param String $title
+   * @param String $original_title
+   * @param String $genre
+   * @param Number $year
+   * @param String $poster
+   * @param String $description
+   */
+
+   public function __construct($_title, $_genre, $_year, $_original_title = null)
+   {
+    $this->title = $_title;
+    $this->original_title = $_original_title;
+    $this->genre = $_genre;
+    $this->year = $_year;
+   }
+
+   public function set_Poster($_poster) {
+    $this->poster = $_poster;
+   }
+
+   public function set_Description() {
+    $this->description = "Il film " . $this->original_title . " è stato realizzato nel " . $this->year . ". Si tratta di un film di genere " . $this->genre;
+   }
+}
+
+$movie_1 = new Movie("La Leggenda del pianista sull'oceano", "drammatico", 1998, "The Legend of 900" );
+$movie_1->set_Poster("https://m.media-amazon.com/images/I/71aWiw3TB-L._AC_SL1200_.jpg");
+$movie_1->set_Description();
+
+$movie_2 = new Movie("Moulin Rouge", "musicale", 2001);
+$movie_2->set_Poster("https://guidatv.sky.it/uuid/9a244376-d700-461a-be95-9fefdcecb303/cover?md5ChecksumParam=ae779f31a869c5435816fdab4c03890d");
+$movie_2->set_Description();
+
+$movie_3 = new Movie("Erin Brockovich - Forte come la verità", "biografico", 2000, "Erin Brockovich");
+
+
+var_dump($movie_1);
+var_dump($movie_2);
+var_dump($movie_3);
+
+?>
