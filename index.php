66c9833afe15d8b3d25d63229f2576dda34becc1
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-07 15:28:31 +0100

    bonus 1: convert genre property of movie class in array
    Movie class accepts more than one genre. To print list of
    genres in decription property, add a get method of class

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ class Movie {
   /**
    * @param String $title
    * @param String $original_title
-   * @param String $genre
+   * @param Array $genre
    * @param Number $year
    * @param String $poster
    * @param String $description
@@ -29,20 +29,31 @@ class Movie {
    }
 
    public function set_Description() {
-    $this->description = "Il film " . $this->original_title . " è stato realizzato nel " . $this->year . ". Si tratta di un film di genere " . $this->genre;
+    $this->description = "Il film " . $this->original_title . " è stato realizzato nel " . $this->year . ". Si tratta di un film di genere " . $this->get_Genres();
+   }
+
+   public function get_Genres() {
+    $genresList = '';
+    $comma = ', ';
+    foreach($this->genre as $key => $single_genre) {
+      if($key === (count($this->genre) - 1)) {
+        $comma = '.';
+      }
+      $genresList .= $single_genre . $comma;
+    }
+    return $genresList;
    }
 }
 
-$movie_1 = new Movie("La Leggenda del pianista sull'oceano", "drammatico", 1998, "The Legend of 900" );
+$movie_1 = new Movie("La Leggenda del pianista sull'oceano", ["drammatico", "musicale", "sentimentale"], 1998, "The Legend of 900" );
 $movie_1->set_Poster("https://m.media-amazon.com/images/I/71aWiw3TB-L._AC_SL1200_.jpg");
 $movie_1->set_Description();
 
-$movie_2 = new Movie("Moulin Rouge", "musicale", 2001);
+$movie_2 = new Movie("Moulin Rouge", ["drammatico", "musicale"], 2001);
 $movie_2->set_Poster("https://guidatv.sky.it/uuid/9a244376-d700-461a-be95-9fefdcecb303/cover?md5ChecksumParam=ae779f31a869c5435816fdab4c03890d");
 $movie_2->set_Description();
 
-$movie_3 = new Movie("Erin Brockovich - Forte come la verità", "biografico", 2000, "Erin Brockovich");
-
+$movie_3 = new Movie("Erin Brockovich - Forte come la verità", ["biografico"], 2000, "Erin Brockovich");
 
 var_dump($movie_1);
 var_dump($movie_2);
